a3f5288c2010b99374fc3892ef4e64c18a059443
Igor Brown <ibrownramos@gmail.com>
2021-11-25 03:49:16 -0300

    finish login page

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 
 import {Text, Image, Alert} from 'react-native';
 
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+
 import { Container, Header, TitleContent, H1, Form, FormControl, Footer } from './styles';
 
 import Logo from '../../../assets/logo1.svg';
@@ -24,6 +26,8 @@ const Login: React.FC = () => {
 
   return (
     <Container>
+
+    <KeyboardAwareScrollView extraHeight={150}>
       <Header colors={['#5E72E4', '#5E72E4']} start={{ x: 0, y: 0}} end={{x: 1, y: 1}}>
         <TitleContent>
           <Logo style={{marginBottom: 40}} />
@@ -35,21 +39,20 @@ const Login: React.FC = () => {
       
       <Form>
         <FormControl>
-          <Input placeholder="Matrícula" onChangeText={(text) => setMatricula(text)} />
-          <Feather size={26} color="#CBD1EB" name="user" style={{  position: 'absolute', zIndex: 99999, right: 16 }} />
+          <Input placeholder="Matrícula" keyboardType='number-pad' onChangeText={(text) => setMatricula(text)} />
+          <Feather size={26} color="#CBD1EB" name="user" style={{ position: 'absolute', zIndex: 99999, right: 16 }} />
         </FormControl>
         <FormControl>
           <Input placeholder="Senha" secureTextEntry={true} onChangeText={(text) => setSenha(text)} />
-          <Feather size={26} color="#CBD1EB" name="lock" style={{  position: 'absolute', zIndex: 99999, right: 16 }} />
+          <Feather size={26} color="#CBD1EB" name="lock" style={{ position: 'absolute', zIndex: 99999, right: 16 }} />
         </FormControl>
         <FormControl>
           <Button title="Entrar na plataforma!" background="primary" onPress={handleLogin} />
         </FormControl>
       </Form>
-      
-
       <Footer>Summa 2021 - Todos os direitos reservasos</Footer>
-      
+    </KeyboardAwareScrollView>
+
     </Container>
   );
 }
